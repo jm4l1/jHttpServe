a9cb927fb9e369fee28c44221cde721871c492d7
Jamal Moore <j_mali26@hotmail.com>
2020-08-26 11:25:38 -0400

    - created cli argument parser for main program

--- main.cpp
+++ main.cpp
@@ -6,158 +6,71 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+#include <string_view>
+#include <regex>
 
 namespace fs = std::filesystem;
 
-int main(){
-    auto file_name = "/Users/mali/Developer/mali/cppfiles/CppND-Capstone-http-server/server.json";
+void print_usage(std::string_view prog_name){
+    std::cout << "Usage: "  << prog_name << " [-f config_file] [-h] [-p port] [-t timeout]" << "\n";
+    std::cout << "\t-f config_file : location of configuration file in .json format\n";
+    std::cout << "\t-p port : Tcp port to accept server connections\n";
+    std::cout << "\t-t timeoutt : Time out of server to close connection \n";
+    std::cout << "\t-h : show help menu\n";
+}
+std::unordered_map<std::string , std::string> parse_argument(int argc , char* argv[] , std::string_view allowed_flags , std::vector<std::string> required_flags){
+    if(argc < 2){
+        std::cout << "Insufficient arguments provided!!\n\n";
+        print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    std::unordered_map<std::string , std::string> argument_map;
+    int count = 1;
+    while(count < (argc - 1)){
+        std::string arg = argv[count];
+        std::regex flag_regex("^-(.)");
+        std::smatch flag_match;
+        if(!std::regex_match(arg, flag_match, flag_regex)){
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+        };
+        if(std::find(allowed_flags.begin(),allowed_flags.end(),flag_match[1]) == allowed_flags.end()){
+            std::cout << "Error : Invalid flag " << arg <<"\n";
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        if(flag_match[1] == "h"){
+            print_usage(argv[0]);
+            exit(EXIT_SUCCESS);
+        }
+        if(count + 1 > argc ){
+            std::cout << "Error : Insufficient arguments provided!!\n\n";
+            print_usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
+        argument_map.insert({flag_match[1],argv[count+1]});
+        count += 2;
+    }
+    for(auto flag : required_flags){
+        if(argument_map.find(flag) == argument_map.end()){
+        std::cout << "Error : Flag -" << flag << " is required\n\n";
+        print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    }
+    return argument_map;
+}
+int main(int argc , char* argv[]){
+    auto argument_map = parse_argument(argc,argv ,"hfpt" ,{"f"});
+    auto file_name = argument_map["f"];
+    auto port = 0;
+    if(argument_map.find("p") != argument_map.end()){
+        port = std::stoi(argument_map["p"]);
+    }
+    auto timeout = 0;
+    if(argument_map.find("t") != argument_map.end()){
+        timeout =std::stoi(argument_map["t"]);
+    }
     auto server = HttpServer();
     server.Init(file_name);
-    // if(!fs::exists(fs::path(file_name)))
-    // {
-    //     std::cout << "Config file could not be found!!\n";
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // std::ifstream config_file(file_name);
-    // if(!config_file.is_open())
-    // {
-    //     std::cout << "Error Opening Config file!\n";
-    //     exit(EXIT_FAILURE);
-    // }
-    
-    // std::string config_string;
-    // std::string line;
-    // while (std::getline(config_file, line)) {
-    //     config_string.append(line);
-    // }
-    // auto config_object = jjson::value::parse_from_string(config_string);
-    // if(!config_object.is_valid()){
-    //     std::cout << "Error Parsing Config file. Please verify valid JSON\n";
-    //     exit(EXIT_FAILURE);
-    // }
-    // if(!config_object.HasKey("port")){
-    //     std::cout << "port is required in config file\n";
-    //     exit(EXIT_FAILURE);
-    // }
-    // if(!config_object.HasKey("web_dir")){
-    //     std::cout << "web_dir location is required in config file\n";
-    //     exit(EXIT_FAILURE);
-    // }
-    // std::string web_dir = (std::string)config_object["web_dir"];
-    // if(!fs::exists(fs::path(web_dir)))
-    // {
-    //     std::cout << "web dir could not be found!\n";
-    //     exit(EXIT_FAILURE);
-    // }
-    // std::cout << "config file loaded\n";
-    // int port = int(config_object["port"]);
-
-    // auto s = SocketServer(port);
-
-    // auto e = HttpResponse(R"(HTTP/1.1 301 Moved Permanently
-    //                 Server: nginx
-    //                 Date: Sat, 22 Aug 2020 17:24:40 GMT
-    //                 Content-Type: text/html
-    //                 Content-Length: 178
-    //                 Connection: keep-alive
-    //                 Location: https://www.goal.com/
-    //                 X-Server-Id: fadda5cf6fb9b00221bdcfc7b43ef36d038a9009
-
-    //                 <html>
-    //                 <head><title>301 Moved Permanently</title></head>
-    //                 <body bgcolor="white">
-    //                 <center><h1>301 Moved Permanently</h1></center>
-    //                 <hr><center>nginx</center>
-    //                 </body>
-    //                 </html>)");
-              
-    // s.Listen([](std::string buffer, std::promise<std::string> &&response_promise)
-    //     {
-    //         std::cout <<  "received message \n---" << buffer <<"---\n";
-    //         response_promise.set_value("Your message was resceived\n");  
-    //     });         
-//     s.Listen([](std::string buffer, std::promise<std::string> &&response_promise)
-//         {
-//             //Parsing Layer handler
-//             auto request = HttpRequest(buffer);
-//             auto response = HttpResponse(std::move(response_promise));
-//             response.SetHeader("Server","CPPND HTTP  Server");
-//             if(!request.isValid){
-//                 response.SetStatusCode(400);
-//                 response.Send();
-//                 return;
-//             }
-//             // auto response = HttpResponse(R"(HTTP/1.1 301 Moved Permanently
-//             //         Server: nginx
-//             //         Date: Sat, 22 Aug 2020 17:24:40 GMT
-//             //         Content-Type: text/html
-//             //         Content-Length: 178
-//             //         Connection: keep-alive
-//             //         Location: https://www.goal.com/
-//             //         X-Server-Id: fadda5cf6fb9b00221bdcfc7b43ef36d038a9009
-
-//             //         <html>
-//             //         <head><title>301 Moved Permanently</title></head>
-//             //         <body bgcolor="white">
-//             //         <center><h1>301 Moved Permanently</h1></center>
-//             //         <hr><center>nginx</center>
-//             //         </body>
-//             //         </html>)"
-//             // );
-//             response.SetBody(R"(<html>
-// <head><title>301 Moved Permanently</title></head>
-// <body bgcolor="white">
-// <center><h1>301 Moved Permanently</h1></center>
-// <hr><center>nginx</center>
-// </body>
-// </html>)");
-//             response.Send();
-//         });
-    
-    // auto rm = RouteMap();
-    // rm.RegisterRoute("GET /" ,[](){return;});
-    // rm.RegisterRoute("POST /" ,[](){return;});
-    // rm.RegisterRoute("GET /home" ,[](){return;});
-    // rm.RegisterRoute("DELETE /" ,[](){return;});
-    // std::cout <<rm.GetRoutes() << "\n";
-    // rm.UnregisterRoute("DELETE /"
-    // );
-    // std::cout <<rm.GetRoutes() << "\n";
-    // rm.UnregisterRoute("DELETE /"
-    // );
-    // std::cout << "the route map " << (rm.HasRoute("GET /") ? "has " : "does not have ") << " the route GET_/ \n"; 
-    // std::cout << "the route map " << (rm.HasRoute("GET /files") ? "has " : "does not have ") << " the route GET_/ \n"; 
-    
-//     auto response = HttpResponse(R"(HTTP/1.1 301 Moved Permanently
-// Server: nginx
-// Date: Sat, 22 Aug 2020 17:24:40 GMT
-// Content-Type: text/html
-// Content-Length: 178
-// Connection: keep-alive
-// Location: https://www.goal.com/
-// X-Server-Id: fadda5cf6fb9b00221bdcfc7b43ef36d038a9009
-
-// <html>
-// <head><title>301 Moved Permanently</title></head>
-// <body bgcolor="white">
-// <center><h1>301 Moved Permanently</h1></center>
-// <hr><center>nginx</center>
-// </body>
-// </html>)");
-//     auto request = HttpRequest(R"(GET / HTTP/1.1
-// Host: goal.com
-// Connection: keep-alive
-// Upgrade-Insecure-Requests: 1
-// User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36
-// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-// Accept-Encoding: gzip, deflate
-// Accept-Language: en-GB,en-US;q=0.9,en;q=0.8)");
-//     std::cout << response.GetStatusCode()<< "\n";
-//     response.SetStatusCode(200);
-//     std::cout << response.GetStatusCode()<< "\n";
-//     std::cout << response.ToString() << "\n";
-//     std::cout << request.ToString() << "\n";
-//     config_file.close();
-//     return 0;
 }
